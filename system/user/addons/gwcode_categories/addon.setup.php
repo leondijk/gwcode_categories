0ea5ccaa75c89ef1108a1ed10768b7101d35961a
dkouvdis <dkouvdis@gmail.com>
2016-08-01 22:20:46 +0100

    Add config to addon setup file

---
+++ system/user/addons/gwcode_categories/addon.setup.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Manners - Addon Setup
+ *
+ * @package        GWcode
+ * @author         Leon Dijk
+ * @copyright      Copyright (c) 2016, Leon Dijk.
+ * @link           https://github.com/leondijk/gwcode_categories
+ * @license        MIT
+ * @version        1.9.0
+ * @filesource     gwcode_categories/addon.setup.php
+ */
+
+
+return array(
+      'author'      => 'Leon Dijk',
+      'author_url'  => 'https://github.com/leondijk',
+      'name'        => 'GWcode',
+      'description' => 'List all, last child categories only or categories of a certain (minimum, maximum, fixed) depth, for a channel, for one or more category groups, for an entry or overall. Show the number of entries in categories, display a list of child categories for a category, create a category based breadcrumb trail and more.',
+      'version'     => '1.9.0',
+      'namespace'   => '\Leondijk\Addons\GWcode',
+);
